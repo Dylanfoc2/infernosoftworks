fad52336d18ac433072fd860300100409ba827e9
Dylanfoc2 <144268338+Dylanfoc2@users.noreply.github.com>
2025-08-28 23:23:32 +0200

    Bot added HWID 76516782-148b-4072-a6f9-5ecc89982d56 to blacklist

--- blacklist.lua
+++ blacklist.lua
@@ -1,5 +1,6 @@
 return {
 "PlaceHolder",
   "9f06df75-c6a9-4eaa-bba5-ecf4a2f43412",
-  "ecbef899-6561-40ec-ac59-46b9a0523284"
+  "ecbef899-6561-40ec-ac59-46b9a0523284",
+  "76516782-148b-4072-a6f9-5ecc89982d56"
 }
